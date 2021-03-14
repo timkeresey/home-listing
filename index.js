1d214fe73276a3c9ee35376ae01489fd54b16f9d
Tim Keresey <tim.keresey@gmail.com>
2021-03-14 17:47:56 -0600

    establish id route params

--- index.js
+++ index.js
@@ -4,16 +4,36 @@ const app = express();
 
 require('dotenv').config();
 
+const homes = [
+    {
+        id: 1,
+        type: 'Apartment',
+        description: 'Well furnished apartment'
+    },
+    {
+        id: 2,
+        type: 'Flat',
+        description: 'Well furnished flat in Denver'  
+    }
+]
+
 app.get('/', (req, res) => {
     res.send('xxx');
 });
 
 app.get('/api/listing', (req, res) => {
-    res.send([
-        {id: 1, roomType: 'Duplex'}, 
-        {id: 2, roomType: 'Flat'}
-    ]);
-})
+    res.send(homes)
+});
+
+app.get('/api/listing/:id', (req, res) => {
+    const home = homes.find(home => home.id === parseInt(req.params.id));
+
+    if (!home) {
+        res.status(404).send('The home with the given ID cannot be found')
+    }
+
+    res.send(home);
+});
 
 const port = process.env.PORT || 3000;
 
