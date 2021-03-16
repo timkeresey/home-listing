196b5e5ae2be8658503daa8373ae79382d56a232
Tim Keresey <tim.keresey@gmail.com>
2021-03-15 19:53:15 -0600

    add validation for house properties

--- routes/houses.js
+++ routes/houses.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const {check, validationResult} = require('express-validator');
 const House = require('../models/House');
 
 const router = express.Router();
 
+const validate = [
+    check('title')
+        .isLength({min: 3, max: 50})
+        .withMessage('Title should be 3 to 50 characters'),
+    check('description')
+        .isLength({min: 10, max: 200})
+        .withMessage('Description should be 10 to 200 characters'),
+    check('address')
+        .isLength({min: 10, max: 100})
+        .withMessage('Address should be 10 to 100 characters'),
+    check('price')
+        .isNumeric()
+        .withMessage('Price should be a number')
+]
+
 // /api/houses
-router.post('/', (req, res) => {
+router.post('/', validate, (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).sen({errors: errors.array()});
+    }
+
     const house = new House({
         title: req.body.title,
         address: req.body.address,
